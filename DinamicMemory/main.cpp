0ddedd51ccbcc05258b36dcafd49d75f88bb980e
Алексей Семионов <alehaaaaa@mail.ru>
2024-05-30 14:46:23 +0300

    Single DynamicArrays DONE

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -6,6 +6,14 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 
+int* push_back(int arr[],int& n,const int value);
+int* push_front(int arr[], int& n, const int value);
+int* insert(int arr[], int& n, const int value, const int index);
+
+int* pop_back(int arr[], int& n);
+int* pop_front(int arr[], int& n);
+int* erase(int arr[], int& n, const int index);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -17,9 +25,33 @@ void main()
 	FillRand(arr, n);
 	Print(arr, n);
 
+	int value;
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr = push_back(arr, n, value);
+	Print(arr, n);
+
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr = push_front(arr, n, value);
+	Print(arr, n);
+
+	int index;
+	cout << "¬ведите индекс добовл€емого значени€: "; cin >> index;
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr = insert(arr, n, value, index);
+	Print(arr, n);
+
+	arr = pop_back(arr, n);
+	Print(arr, n);
+
+	arr = pop_front(arr, n);
+	Print(arr, n);
+
+	cout << "¬ведите индекс удол€емого значени€: "; cin >> index;
+	arr = erase(arr, n, index);
+	Print(arr, n);
+
 	delete[] arr;
 	// Memory leaks
-
 }
 
 void FillRand(int arr[], const int n)
@@ -39,4 +71,84 @@ void Print(int arr[], const int n)
 		//ќбращение к элементам массива через оператор индексировани€ []
 	}
 	cout << endl;
+}
+
+int* push_back(int arr[],int& n, const int value)
+{
+	//1)создаем буферный массив нужного размера
+	int* buffer = new int[n + 1];
+	//2)копируем значени€ исходного массива в буферный
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i];
+	}
+	//3)удал€ем исходный массив
+	delete[] arr;
+	//4)подмен€ем адрес в указателе arr адресом нового массива
+	int a = 2;
+	int b = 3;
+	a = b;
+	arr = buffer;
+	//5)только после всего этого можно добавить значение в конец массива
+	arr[n] = value;
+	//6)после добаавлени€ элимента в конеуц массива колво его элементов увеличиваетс€ на 1
+	n++;
+	return arr;
+}
+
+int* push_front(int arr[], int& n, const int value)
+{
+	int* buffer = new int[n + 1];
+	for (int i = 0; i < n; i++)buffer[i + 1] = arr[i];
+	delete[] arr;
+	buffer[0] = value;
+	n++;
+	return buffer;
+}
+
+int* insert(int arr[], int& n, const int value, const int index)
+{
+	int* buffer = new int[n + 1];
+	//for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	//for (int i = index; i < n; i++)buffer[i + 1] = arr[i];
+	for (int i = 0; i < n; i++)
+	{
+		//if (i < index)buffer[i] = arr[i];
+		//else buffer[i + 1] = arr[i];
+		//i < index ? buffer[i] = arr[i] : buffer[i + 1] = arr[i];
+		buffer[i < index ? i : i + 1] = arr[i];
+	}
+	delete[] arr;
+	buffer[index] = value;
+	n++;
+	return buffer;
+}
+
+int* pop_back(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	delete[] arr;
+	return buffer;
+}
+
+int* pop_front(int arr[], int& n)
+{
+	int* buffer = new int[--n];
+	for (int i = 0; i < n; i++)buffer[i] = arr[i + 1];
+	delete[] arr;
+	return buffer;
+}
+
+int* erase(int arr[], int& n, const int index)
+{
+	int* buffer = new int[--n];
+	//for (int i = 0; i < index; i++)buffer[i] = arr[i];
+	//for (int i = index; i < n; i++)buffer[i] = arr[i + 1];
+	for (int i = 0; i < n; i++)
+	{
+		buffer[i] = arr[i < index ? i : i + 1];
+	}
+	delete[] arr;
+	return buffer;
 }
